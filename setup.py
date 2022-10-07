0dfbbf1020fbf76b7bc092f17b7b81a648376616
OJO Adebowale Suleiman <suleimanojo3@gmail.com>
2022-10-07 12:02:56 +0200

    pip install the distribution package

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='probability_distributions',
+setup(name='distributions',
       version='1.0',
       description=['Gaussian distributions','Binomial distributions'],
-      packages=['probability_distributions'],
+      packages=['distributions'],
       author='OJO Adebowale',
       author_email= 'suleimanojo3@gmail.com',
       zip_safe=False
